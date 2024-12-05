344afb8be0560fd9620a7e977b5544b7c5d7d6e7
CY <cy823236858@gmail.com>
2024-12-05 21:52:33 +0800

    whisper_test

--- scripts/train_stage2_loppy_whisper.py
+++ scripts/train_stage2_loppy_whisper.py
@@ -459,10 +459,9 @@ def log_validation(
             audio_fea_final = torch.Tensor(whisper_chunks)
             print("audio_fea_final shape ", audio_fea_final.shape)
             audio_length = audio_fea_final.shape[0] - 1
-            audio_tensor = audio_fea_final[
-                           t * clip_length: min((t + 1) * clip_length, audio_emb.shape[0]-1)
+            audio_tensor = audio_fea_final[:,t * clip_length: min((t + 1) * clip_length, audio_emb.shape[0]-1)
                            ]
-
+            audio_tensor = audio_tensor.unsqueeze(0)
             audio_tensor = audio_tensor.to(
                 device=audioproj.device, dtype=audioproj.dtype)
             audio_tensor = audioproj(audio_tensor)
