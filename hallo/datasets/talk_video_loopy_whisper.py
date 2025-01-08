4f90dec941f7c09765ac3faf3b61f54975b829af
CY <cy823236858@gmail.com>
2025-01-08 09:45:06 +0800

    svd update diffuser

--- hallo/datasets/talk_video_loopy_whisper.py
+++ hallo/datasets/talk_video_loopy_whisper.py
@@ -370,7 +370,8 @@ class TalkingVideoDataset(Dataset):
                 )
 
             audio_tensor_whisper = audio_tensor_whisper.squeeze(0)
-            # print("\n audio_tensor shape is :", audio_tensor_whisper.shape)
+            print("\n audio_tensor shape is :", audio_tensor_whisper.shape)
+            print("pixel_values_vid shape is :", pixel_values_vid.shape)
 
             sample = {
                 "start_idx": start_idx,
