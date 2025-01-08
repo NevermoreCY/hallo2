22f582f7a5de9f73d178d3fc1ce8f598ed119d1f
CY <cy823236858@gmail.com>
2025-01-08 10:12:31 +0800

    svd update diffuser

--- hallo/datasets/talk_video_loopy_whisper.py
+++ hallo/datasets/talk_video_loopy_whisper.py
@@ -358,9 +358,11 @@ class TalkingVideoDataset(Dataset):
                 self.augmentation(full_masks_list, self.attn_transform_16, state),
                 self.augmentation(full_masks_list, self.attn_transform_8, state),
             ]
-
+            print("pixel_values_ref_img shape is 1", ref_img.shape)
             pixel_values_ref_img = self.augmentation(ref_img, self.pixel_transform, state)
+            print("pixel_values_ref_img shape is 2", pixel_values_ref_img.shape)
             pixel_values_ref_img = pixel_values_ref_img.unsqueeze(0)
+            print("pixel_values_ref_img shape is 3", pixel_values_ref_img.shape)
             if self.n_motion_frames > 0:
                 pixel_values_motion = self.augmentation(
                     motion_list, self.pixel_transform, state
@@ -372,6 +374,7 @@ class TalkingVideoDataset(Dataset):
             audio_tensor_whisper = audio_tensor_whisper.squeeze(0)
             print("\n audio_tensor shape is :", audio_tensor_whisper.shape)
             print("pixel_values_vid shape is :", pixel_values_vid.shape)
+            print("pixel_values_ref_img shape is ", pixel_values_ref_img.shape)
 
             sample = {
                 "start_idx": start_idx,
