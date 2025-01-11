15edc3d3c533f4c0b19933a278de977b0aecddad
CY <cy823236858@gmail.com>
2025-01-11 15:19:20 +0800

    svd update diffuser

--- hallo/models/diffuser/unet_spatio_temporal_condition_audio.py
+++ hallo/models/diffuser/unet_spatio_temporal_condition_audio.py
@@ -455,9 +455,9 @@ class UNetSpatioTemporalConditionModel(ModelMixin, ConfigMixin, UNet2DConditionL
         # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
         batch_size, num_frames = sample.shape[:2]
         timesteps = timesteps.expand(batch_size)
-        # print(timesteps,timesteps.shape)
+        print(timesteps,timesteps.shape)
         t_emb = self.time_proj(timesteps)
-        # print("t_emb shape is ", t_emb.shape)
+        print("t_emb shape is ", t_emb.shape)
         # input sample shape 0 :  torch.Size([4, 14, 4, 64, 64])
         # time steps shape :  torch.Size([4]) False
         # tensor([0.2014, 0.3099, 0.0070, 0.7441], device='cuda:0') torch.Size([4])
@@ -466,7 +466,7 @@ class UNetSpatioTemporalConditionModel(ModelMixin, ConfigMixin, UNet2DConditionL
         # `Timesteps` does not contain any weights and will always return f32 tensors
         # but time_embedding might actually be running in fp16. so we need to cast here.
         # there might be better ways to encapsulate this.
-        # print("t_emb dtype is ", t_emb.dtype)
+        print("t_emb dtype is ", t_emb.dtype)
         # print("sample dtype is ", sample.dtype)
         # t_emb dtype is  torch.float32
         # sample dtype is  torch.float16
