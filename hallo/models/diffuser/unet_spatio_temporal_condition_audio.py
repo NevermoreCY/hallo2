acc142dba26462a447343c1ce71c26ba3b4f65a8
CY <cy823236858@gmail.com>
2025-01-15 00:48:00 +0800

    svd update diffuser

--- hallo/models/diffuser/unet_spatio_temporal_condition_audio.py
+++ hallo/models/diffuser/unet_spatio_temporal_condition_audio.py
@@ -472,6 +472,7 @@ class UNetSpatioTemporalConditionModel(ModelMixin, ConfigMixin, UNet2DConditionL
         # t_emb dtype is  torch.float32
         # sample dtype is  torch.float16
         t_emb = t_emb.to(dtype=audio_embedding.dtype)
+        sample = sample.to(dtype=audio_embedding.dtype)
         print("**\n\n "
               "sample dtype is ", sample.dtype)
         print("t_emb dtype is ", t_emb.dtype)
