abdebf423cc98990e437e63e0e1bd636c988c5f6
CY <cy823236858@gmail.com>
2025-01-16 15:16:49 +0800

    svd update diffuser

--- hallo/animate/pipeline_svd_whisper.py
+++ hallo/animate/pipeline_svd_whisper.py
@@ -667,7 +667,7 @@ class StableVideoDiffusionPipeline(DiffusionPipeline):
                     print("image_embeddings_cfg",image_embeddings_cfg.dtype)
 
                     # predict the noise residual
-                    noise_pred = self.unet(
+                    pred = self.unet(
                         latent_model_input,
                         t,
                         encoder_hidden_states=image_embeddings_cfg if self.do_classifier_free_guidance else image_embeddings,
@@ -676,17 +676,15 @@ class StableVideoDiffusionPipeline(DiffusionPipeline):
                         return_dict=False,
                     )[0]
 
-                    # model_pred = self.denoising_unet(
-                    #     noisy_latents,
-                    #     timesteps,
-                    #     encoder_hidden_states=face_emb,
-                    #     audio_embedding=audio_emb,
-                    #     added_time_ids=added_time_ids,
-                    # ).sample
+                    for j, c in enumerate(new_context):
+                        noise_pred[:, :, c] = noise_pred[:, :, c] + pred
+                        counter[:, :, c] = counter[:, :, c] + 1
 
+                print("check counter", counter)
                 # perform guidance
                 if self.do_classifier_free_guidance:
-                    noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+                    # noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+                    noise_pred_uncond, noise_pred_cond = (noise_pred / counter).chunk(2)
                     noise_pred = noise_pred_uncond + self.guidance_scale * (noise_pred_cond - noise_pred_uncond)
 
                 # compute the previous noisy sample x_t -> x_t-1
@@ -702,6 +700,10 @@ class StableVideoDiffusionPipeline(DiffusionPipeline):
 
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
                     progress_bar.update()
+
+
+
+
 
         if not output_type == "latent":
             # cast back to fp16 if needed
